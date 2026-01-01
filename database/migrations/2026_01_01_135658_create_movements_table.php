2d8c7ef70d1bf68d12a759ad5f837171f9cc4126
LC-jhony <jhonyapm94@gmail.com>
2026-01-01 10:39:01 -0500

    update migration movement

--- database/migrations/2026_01_01_135658_create_movements_table.php
+++ database/migrations/2026_01_01_135658_create_movements_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('movements', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('document_id');
+            $table->string('transaction_number');
+            $table->unsignedBigInteger('origin_office_id');
+            $table->unsignedBigInteger('origin_user_id');
+            $table->unsignedBigInteger('destination_office_id');
+            $table->unsignedBigInteger('destination_user_id');
+            $table->string('action');
+            $table->string('indication');
+            $table->string('observation');
+            $table->string('urgent_priority');
+            $table->date('movement_date');
+            $table->date('receipt_date');
+            $table->date('service_date');
+            $table->boolean('status');
+            $table->boolean('requires_response');
+            $table->boolean('is_copy');
             $table->timestamps();
         });
     }
